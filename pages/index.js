f76d2c482a075b02a4730105a3c7d0803d50ebed
Sakol Assawasagool <koobitor@gmail.com>
2017-10-24 23:13:40 +0700

    change default page

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,19 @@
 import Layout from '../components/Layout'
 
-export default () => (
-  <Layout>
-    <div className="container">
-      <div>Hello World.</div>
-    </div>
-  </Layout>
-)
+class Page extends React.Component {
+
+  render() {
+    return (
+      <Layout>
+        <div class="container">
+          <div class="starter text-center">
+            <h1>Bootstrap starter template</h1>
+            <p class="lead">Use this document as a way to quickly start any new project.<br/> All you get is this text and a mostly barebones HTML document.</p>
+          </div>
+        </div>
+      </Layout>
+    )
+  }
+}
+
+export default Page
